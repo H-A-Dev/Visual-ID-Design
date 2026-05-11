554b36a6c6c027bd3aa8f6aa61405d2fc81872fa
H.A Dev <49473600+H-A-Dev@users.noreply.github.com>
2026-05-11 21:05:33 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
+import streamlit as st
+
+# إعداد الصفحة
+st.set_page_config(page_title="هوية سياحة المملكة", layout="wide")
+
+# عنوان مبدئي
+st.title("لوحة تحكم الهوية البصرية - لون الخزامى")
+st.write("أهلاً بكِ! هنا سنعرض أكواد الهوية التي صممناها.")
+
+# زر لاختبار الألوان
+color = st.color_picker("جربي درجة لون الخزامى", "#967BB6")
+st.write(f"الدرجة الحالية هي: {color}")
 
